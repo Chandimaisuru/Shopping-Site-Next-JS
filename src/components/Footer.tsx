f7bf6d244f4790e9f97b3480d7147bb67bfde382
Chandimaisuru <isuruchandima92@gmail.com>
2024-02-23 13:07:11 +0530

    Footer done

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className=' bg-blackish text-gray-500 text-center py-4 pb-16 md:pb-4'>Copyright @ Marix <br />All Rights Reserved </div>
+  )
+}
+
+export default Footer
