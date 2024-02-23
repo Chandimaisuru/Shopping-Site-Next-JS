56188002f66f1e5e7683fe7d32abe4c6951eeca1
Chandimaisuru <isuruchandima92@gmail.com>
2024-02-23 12:47:34 +0530

    Testimonials done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import NewProducts from "@/components/NewProducts";
 import Hero from "../components/Hero"
+import Testimonial from "@/components/Testimonial";
 
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
    <main>
     <Hero/>
     <NewProducts/>
+    <Testimonial/>
    </main>
    
   );
